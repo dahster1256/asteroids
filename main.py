820da0a4ce1f1b6ceddd896fb91b67ffc59ae86f
dahster1256 <90005100+dahster1256@users.noreply.github.com>
2024-12-04 11:04:26 -0500

    exercise 8

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
@@ -18,12 +21,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for obj in updatable:
+            obj.update(dt)
+            
         screen.fill(color="black")
-        dt += clock.tick(60) / 1000
-        player.draw(screen)
-        player.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+    
         pygame.display.flip()
-       
+        dt += clock.tick(60) / 1000
     
 if __name__ == "__main__":
     main()
